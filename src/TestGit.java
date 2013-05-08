3f775c5ab99162e4ab2247ba8cfa82335bb31a22
tony <sunytonyli@gmail.com>
2013-05-08 11:06:07 +0800

    TestGit-v2-main

---
+++ src/TestGit.java
@@ -0,0 +1,14 @@
+/**
+ * @author Tony
+ * @createDatime 2013-5-8 上午11:04:28
+ */
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+
+}
